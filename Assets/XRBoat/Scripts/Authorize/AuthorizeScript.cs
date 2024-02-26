884ff424dc0989f1e9e077622926d5b4272274f3
Frank92613 <yan92613@gmail.com>
2024-02-27 00:30:44 +0800

    2024/02/27更新
    授權字幕搞定

--- Assets/XRBoat/Scripts/Authorize/AuthorizeScript.cs
+++ Assets/XRBoat/Scripts/Authorize/AuthorizeScript.cs
@@ -6,20 +6,26 @@ public class AuthorizeScript : MonoBehaviour
 {
     public GameObject AuthorizeTtile;
     public Vector3 moveDirection = Vector3.up;
-    private float moveSpeed = 20.0f;
-    public bool isTriggered = false; //Ä²µoª¬ºA
+    public float moveSpeed = 20.0f;
+    public bool isTriggered = false; //Ä²ï¿½oï¿½ï¿½ï¿½A
+    
 
-    // Update is called once per frame
-  
+    private void Start()
+    {
+        
+        
+    }
+
     void Update()
     {
          Debug.Log(isTriggered);
         
         if (isTriggered) { 
 
-            float distance = Time.deltaTime * moveSpeed;
-            AuthorizeTtile.transform.Translate(moveDirection * distance);
+            float distance = moveSpeed * Time.deltaTime;
+            AuthorizeTtile.transform.Translate(moveSpeed*moveDirection * distance);
         }
+            
     }
 
 
